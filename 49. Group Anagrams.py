c4d6152e91fcabf010cc1c670b38dd6c43842093
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-16 17:13:16 -0600

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,3 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        print("hi")
+        n = len(strs)
+        hashMap = {}
+        for i in range(n):
+            # sorted anagrams by alphabetical order will be indentical, so just group them as list in map then return
+            sortedWord = "".join(sorted(strs[i]))
+            hashMap[sortedWord] = [strs[i]] + hashMap.get(sortedWord, [])
+        return list(hashMap.values())
