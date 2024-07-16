6f06fa95879d3f3f81374a6925a0517dfbe003fb
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-15 22:40:59 -0600

    Update 217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,12 +1,15 @@
 # checks to see if there is a duplicate integar in an array
 
 
-# using hashmap
+# using dictionary
 class Solution:
+    # makes it so you only have to iterate through array one time to check
     def containsDuplicate(self, nums: list[int]) -> bool:
         seen = {}
         for num in nums:
-            if num in seen and seen[num] >= 1:
+            if num in seen and seen[num] >= 1:  # checking if it is in dictionary
                 return True
-            seen[num] = seen.get(num, 0) + 1
+            seen[num] = (
+                seen.get(num, 0) + 1
+            )  # adding a count to keep track of how many times it has come up
         return False
