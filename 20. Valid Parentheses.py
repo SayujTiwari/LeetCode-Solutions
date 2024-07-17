5dcc9b1e07ccda2fc519cc385c23e41ce17b1371
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-16 21:53:03 -0600

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,6 +1,24 @@
 class Solution:
+    def __init__(self) -> None:
+        self.stack = []
+
+    def pop(self):
+        self.stack.pop(-1)
+
+    def push(self, item):
+        self.stack.append(item)
+
     def isValid(self, s: str) -> bool:
-        stack = []
+        for letter in s:
+            if letter == "(" or letter == "{" or letter == "[":
+                self.push(s)
+            else:
+                self.pop()
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
 
 
 s = "()"
+print(Solution().isValid(s))
