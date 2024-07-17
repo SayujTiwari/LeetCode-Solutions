dcace8df8fef92248105dee9909d7dc213c5e6ef
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-16 22:49:57 -0600

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,4 +1,5 @@
 class Solution:
+    # create Stack DS
     def __init__(self) -> None:
         self.stack = []
 
@@ -9,28 +10,26 @@ class Solution:
     def push(self, item):
         self.stack.append(item)
 
+    # checking Function
     def isValid(self, s: str) -> bool:
         if len(s) % 2 != 0:
             return False
         for letter in s:
-            print(self.stack)
-            print(letter)
             if letter in "({[":
                 self.push(letter)
+            # checking each case
             elif len(self.stack) > 0:
                 if letter == ")" and self.stack[-1] == "(":
                     self.pop()
-                if letter == "}" and self.stack[-1] == "{":
+                elif letter == "}" and self.stack[-1] == "{":
                     self.pop()
-                if letter == "]" and self.stack[-1] == "[":
+                elif letter == "]" and self.stack[-1] == "[":
                     self.pop()
+                else:
+                    return False
             else:
                 return False
         if len(self.stack) == 0:
             return True
         else:
             return False
-
-
-s = "()"
-print(Solution().isValid(s))
