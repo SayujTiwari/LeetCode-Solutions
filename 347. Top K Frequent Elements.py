75dd3bbe5725e954626246249321bdd036c3f764
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-16 23:47:32 -0600

    Update 347. Top K Frequent Elements.py

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,3 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        pass
+        hashMap = {}
+        for num in nums:
+            hashMap[num] = 1 + hashMap.get(num, 0)
+        # sort the map from highest to lowest frequency
+        sortedOne = list(
+            sorted(hashMap.items(), key=lambda item: item[1], reverse=True)
+        )
+        output = []
+        # looping to output correct amount
+        for i in range(k):
+            output.append(sortedOne[i][0])
+        return output
