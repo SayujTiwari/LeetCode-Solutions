72bc788243d297e99254a12602829dc39d732de1
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-16 22:46:53 -0600

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -3,7 +3,7 @@ class Solution:
         self.stack = []
 
     def pop(self):
-        item = self.stack.pop(-1)
+        item = self.stack.pop()
         return item
 
     def push(self, item):
@@ -17,30 +17,20 @@ class Solution:
             print(letter)
             if letter in "({[":
                 self.push(letter)
-            elif letter in ")}]":
-                item = self.pop()
-                if item != letter:
-                    return False
+            elif len(self.stack) > 0:
+                if letter == ")" and self.stack[-1] == "(":
+                    self.pop()
+                if letter == "}" and self.stack[-1] == "{":
+                    self.pop()
+                if letter == "]" and self.stack[-1] == "[":
+                    self.pop()
+            else:
+                return False
         if len(self.stack) == 0:
             return True
         else:
             return False
-                
-            # elif letter == ")" and len(self.stack) > 0:
-            #     item = self.pop()
-            #     if item != ""
-            # elif letter == "}" and len(self.stack) > 0:
-            #     item = self.pop()
-            # elif letter == "]" and len(self.stack) > 0:
-            #     item = self.pop()
-                
-            else:
-                return False
-        if len(self.stack) != 0:
-            return False
-        else:
-            return True
 
 
-s = "){"
+s = "()"
 print(Solution().isValid(s))
