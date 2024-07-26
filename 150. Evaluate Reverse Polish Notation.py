bc0ad681181d756ccd20b92610e1fa1f13af423d
SayujTiwari <tiwarisayuj@gmail.com>
2024-07-26 11:03:09 -0600

    Update 150. Evaluate Reverse Polish Notation.py

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.stack) == 1:
+            self.min.append(val)
+        else:
+            if val <= self.min[len(self.min) - 1]:
+                self.min.append(val)
+
+    def pop(self) -> None:
+        if self.stack[len(self.stack) - 1] == self.min[len(self.min) - 1]:
+            self.min.pop()
+        self.stack.pop()
+
+    def evalRPN(self, tokens: list[str]) -> int:
+        pass
