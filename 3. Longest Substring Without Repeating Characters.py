692537b70f08c02fa3fd5f4edc8384199da84297
SayujTiwari <tiwarisayuj@gmail.com>
2024-08-13 10:49:19 -0600

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -6,9 +6,9 @@ class Solution:
         for x in range(len(s)):  # loop through the string
             while (
                 s[x] in charSet
-            ):  # keep removing the other side of the window until reaches dupe, so that substring is a continous one
+            ):  # keep removing the other side of the window until reaches dupe
                 charSet.remove(s[l])
-                l += 1
+                l += 1  # keep moving the left side of the str up to remove the correct
             charSet.add(s[x])
             result = max(result, x - l + 1)
         return result
